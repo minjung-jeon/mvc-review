fe6ba795b7535d67ffe79fe1474dd393c9cf089a
minjung.sw <37206904+minjung-jeon@users.noreply.github.com>
2019-12-12 15:01:19 +0900

    [js] CountModel setter, getter 수정

--- js/models/countModel.js
+++ js/models/countModel.js
@@ -1,41 +1,33 @@
-import Observable from '../lib/observable.js';
-
-export class CountModel extends Observable {
+export class CountModel {
 	constructor() {
-    super();
+    		super();
 
-		this.active = 0;
-		this.done = 0;
-		this.all = 0;
+		this._active = 0;
+		this._done = 0;
+		this._all = 0;
 	}
 
-	// set all(count) {
-  //   if(count){
-  //     this._all = count;
-  //   }
-	// }
+	set all(count) {
+	    	this._all = count;
+	}
 
-	// get all() {
-	// 	return this._all;
-	// }
+	get all() {
+		return this._all;
+	}
 
-	// set active(count) {
-  //   if(count){
-  //     this._active = count;
-  //   }
-	// }
+	set active(count) {
+	      	this._active = count;
+	}
 
-	// get active() {
-	// 	return this._active;
-	// }
+	get active() {
+		return this._active;
+	}
 
-	// set done(count) {
-  //   if(count){
-  //     this._done = count;
-  //   }
-	// }
+	set done(count) {
+		this._done = count;
+	}
 
-	// get done() {
-	// 	return this._done;
-	// }
+	get done() {
+		return this._done;
+	}
 }
